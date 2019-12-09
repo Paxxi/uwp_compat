7fb1faaa6c86de93edc2f9fe63e84d8ccd25c8ab
Pär Björklund <per.bjorklund@gmail.com>
2019-12-09 18:30:35 +0100

    comment wip

--- src/path.cpp
+++ src/path.cpp
@@ -90,143 +90,143 @@ void uwp_fix_slash_u16(wchar_t* str, size_t length)
   }
 }
 
-wchar_t* uwp_get_documents_u16()
-{
-  return hstring_to_wstring(KnownFolders::GetFolderForUserAsync(nullptr, KnownFolderId::DocumentsLibrary).get().Path());
-  return hstring_to_wstring(KnownFolders::DocumentsLibrary().Path());
-}
+// wchar_t* uwp_get_documents_u16()
+// {
+//   return hstring_to_wstring(KnownFolders::GetFolderForUserAsync(nullptr, KnownFolderId::DocumentsLibrary).get().Path());
+//   return hstring_to_wstring(KnownFolders::DocumentsLibrary().Path());
+// }
 
-char* uwp_get_documents_u8()
-{
-  return hstring_to_cstring(KnownFolders::DocumentsLibrary().Path());
-}
+// char* uwp_get_documents_u8()
+// {
+//   return hstring_to_cstring(KnownFolders::DocumentsLibrary().Path());
+// }
 
-wchar_t* uwp_get_cameraroll_u16()
-{
-  return hstring_to_wstring(KnownFolders::CameraRoll().Path());
-}
+// wchar_t* uwp_get_cameraroll_u16()
+// {
+//   return hstring_to_wstring(KnownFolders::CameraRoll().Path());
+// }
 
-char* uwp_get_cameraroll_u8()
-{
-  return hstring_to_cstring(KnownFolders::CameraRoll().Path());
-}
+// char* uwp_get_cameraroll_u8()
+// {
+//   return hstring_to_cstring(KnownFolders::CameraRoll().Path());
+// }
 
-wchar_t* uwp_get_playlists_u16()
-{
-  return hstring_to_wstring(KnownFolders::Playlists().Path());
-}
+// wchar_t* uwp_get_playlists_u16()
+// {
+//   return hstring_to_wstring(KnownFolders::Playlists().Path());
+// }
 
-char* uwp_get_playlists_u8()
-{
-  return hstring_to_cstring(KnownFolders::Playlists().Path());
-}
+// char* uwp_get_playlists_u8()
+// {
+//   return hstring_to_cstring(KnownFolders::Playlists().Path());
+// }
 
-wchar_t* uwp_get_savedpictures_u16()
-{
-  return hstring_to_wstring(KnownFolders::SavedPictures().Path());
-}
+// wchar_t* uwp_get_savedpictures_u16()
+// {
+//   return hstring_to_wstring(KnownFolders::SavedPictures().Path());
+// }
 
-char* uwp_get_savedpictures_u8()
-{
-  return hstring_to_cstring(KnownFolders::SavedPictures().Path());
-}
+// char* uwp_get_savedpictures_u8()
+// {
+//   return hstring_to_cstring(KnownFolders::SavedPictures().Path());
+// }
 
-wchar_t* uwp_get_music_u16()
-{
-  return hstring_to_wstring(KnownFolders::MusicLibrary().Path());
-}
+// wchar_t* uwp_get_music_u16()
+// {
+//   return hstring_to_wstring(KnownFolders::MusicLibrary().Path());
+// }
 
-char* uwp_get_music_u8()
-{
-  return hstring_to_cstring(KnownFolders::MusicLibrary().Path());
-}
+// char* uwp_get_music_u8()
+// {
+//   return hstring_to_cstring(KnownFolders::MusicLibrary().Path());
+// }
 
-wchar_t* uwp_get_pictures_u16()
-{
-  return hstring_to_wstring(KnownFolders::PicturesLibrary().Path());
-}
+// wchar_t* uwp_get_pictures_u16()
+// {
+//   return hstring_to_wstring(KnownFolders::PicturesLibrary().Path());
+// }
 
-char* uwp_get_pictures_u8()
-{
-  return hstring_to_cstring(KnownFolders::PicturesLibrary().Path());
-}
+// char* uwp_get_pictures_u8()
+// {
+//   return hstring_to_cstring(KnownFolders::PicturesLibrary().Path());
+// }
 
-wchar_t* uwp_get_videos_u16()
-{
-  return hstring_to_wstring(KnownFolders::VideosLibrary().Path());
-}
+// wchar_t* uwp_get_videos_u16()
+// {
+//   return hstring_to_wstring(KnownFolders::VideosLibrary().Path());
+// }
 
-char* uwp_get_videos_u8()
-{
-  return hstring_to_cstring(KnownFolders::VideosLibrary().Path());
-}
+// char* uwp_get_videos_u8()
+// {
+//   return hstring_to_cstring(KnownFolders::VideosLibrary().Path());
+// }
 
-wchar_t* uwp_get_homegroup_u16()
-{
-  return hstring_to_wstring(KnownFolders::HomeGroup().Path());
-}
+// wchar_t* uwp_get_homegroup_u16()
+// {
+//   return hstring_to_wstring(KnownFolders::HomeGroup().Path());
+// }
 
-char* uwp_get_homegroup_u8()
-{
-  return hstring_to_cstring(KnownFolders::HomeGroup().Path());
-}
+// char* uwp_get_homegroup_u8()
+// {
+//   return hstring_to_cstring(KnownFolders::HomeGroup().Path());
+// }
 
-wchar_t* uwp_get_removabledevices_u16()
-{
-  return hstring_to_wstring(KnownFolders::RemovableDevices().Path());
-}
+// wchar_t* uwp_get_removabledevices_u16()
+// {
+//   return hstring_to_wstring(KnownFolders::RemovableDevices().Path());
+// }
 
-char* uwp_get_removabledevices_u8()
-{
-  return hstring_to_cstring(KnownFolders::RemovableDevices().Path());
-}
+// char* uwp_get_removabledevices_u8()
+// {
+//   return hstring_to_cstring(KnownFolders::RemovableDevices().Path());
+// }
 
-wchar_t* uwp_get_mediaserverdevices_u16()
-{
-  return hstring_to_wstring(KnownFolders::MediaServerDevices().Path());
-}
+// wchar_t* uwp_get_mediaserverdevices_u16()
+// {
+//   return hstring_to_wstring(KnownFolders::MediaServerDevices().Path());
+// }
 
-char* uwp_get_mediaserverdevices_u8()
-{
-  return hstring_to_cstring(KnownFolders::MediaServerDevices().Path());
-}
+// char* uwp_get_mediaserverdevices_u8()
+// {
+//   return hstring_to_cstring(KnownFolders::MediaServerDevices().Path());
+// }
 
-wchar_t* uwp_get_objects3d_u16()
-{
-  return hstring_to_wstring(KnownFolders::Objects3D().Path());
-}
+// wchar_t* uwp_get_objects3d_u16()
+// {
+//   return hstring_to_wstring(KnownFolders::Objects3D().Path());
+// }
 
-char* uwp_get_objects3d_u8()
-{
-  return hstring_to_cstring(KnownFolders::Objects3D().Path());
-}
+// char* uwp_get_objects3d_u8()
+// {
+//   return hstring_to_cstring(KnownFolders::Objects3D().Path());
+// }
 
-wchar_t* uwp_get_appcaptures_u16()
-{
-  return hstring_to_wstring(KnownFolders::AppCaptures().Path());
-}
+// wchar_t* uwp_get_appcaptures_u16()
+// {
+//   return hstring_to_wstring(KnownFolders::AppCaptures().Path());
+// }
 
-char* uwp_get_appcaptures_u8()
-{
-  return hstring_to_cstring(KnownFolders::AppCaptures().Path());
-}
+// char* uwp_get_appcaptures_u8()
+// {
+//   return hstring_to_cstring(KnownFolders::AppCaptures().Path());
+// }
 
-wchar_t* uwp_get_recordedcalls_u16()
-{
-  return hstring_to_wstring(KnownFolders::RecordedCalls().Path());
-}
+// wchar_t* uwp_get_recordedcalls_u16()
+// {
+//   return hstring_to_wstring(KnownFolders::RecordedCalls().Path());
+// }
 
-char* uwp_get_recordedcalls_u8()
-{
-  return hstring_to_cstring(KnownFolders::RecordedCalls().Path());
-}
+// char* uwp_get_recordedcalls_u8()
+// {
+//   return hstring_to_cstring(KnownFolders::RecordedCalls().Path());
+// }
 
-wchar_t* uwp_get_cookies_u16()
-{
-  return hstring_to_wstring(AppDataPaths::Cookies().Path());
-}
+// wchar_t* uwp_get_cookies_u16()
+// {
+//   return hstring_to_wstring(AppDataPaths::Cookies().Path());
+// }
 
-char* uwp_get_cameraroll_u8()
-{
-  return hstring_to_cstring(KnownFolders::CameraRoll().Path());
-}
+// char* uwp_get_cameraroll_u8()
+// {
+//   return hstring_to_cstring(KnownFolders::CameraRoll().Path());
+// }
